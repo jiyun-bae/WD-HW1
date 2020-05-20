9bd3de54ef91989bc18567df6c1fc1926ca918df
Jiyun <baejiyun01@gmail.com>
2020-05-21 02:49:41 +0900

    My First File

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// console.log("JS file is linked");
+
+//let firstName = "Marco";
+//let age = 40;
+
+//ES6 template literals
+//        console.log('Hello ${firstName} 
+// DeLuca');
+
+//console.log("Hello" + firstName);
